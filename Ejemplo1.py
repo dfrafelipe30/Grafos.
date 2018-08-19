40c0aa4636edb0674ebf2d883ae069d4e8877e45
dfrafelipe30 <33558046+dfrafelipe30@users.noreply.github.com>
2018-08-19 16:40:51 -0500

    Update Ejemplo1.py

--- Ejemplo1.py
+++ Ejemplo1.py
@@ -3,6 +3,13 @@ from Graph import Graph
 G = Graph(["a","b","c"],[("a","b"),("b","c")])
 print G.AdjacencyMatrix()
 
-H = Graph(["d","e","f"], [("a","b"),("e","f")])
+J = Graph(["a","b","c","d"],[("a","b"),("b","c"),("c","d")])
+print J.AdjacencyMatrix()
 
-print H.AdjacencyMatrix()
+K =Graph(["e","f","g","h"],[("e","g"),("g","f"),("f","h")])
+print K.AdjacencyMatrix()
+
+
+#Grafo que  genera error.
+#H = Graph(["d","e","f"], [("a","b"),("e","f")])
+#print H.AdjacencyMatrix()
