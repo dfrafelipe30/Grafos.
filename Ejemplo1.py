e415e6f019b48c7cc769907b0483cdd74a3908c9
dfrafelipe30 <33558046+dfrafelipe30@users.noreply.github.com>
2018-08-19 17:40:44 -0500

    Update Ejemplo1.py

--- Ejemplo1.py
+++ Ejemplo1.py
@@ -1,15 +1,16 @@
 from Graph import Graph
-
+#Grafo de ejemplo.
 G = Graph(["a","b","c"],[("a","b"),("b","c")])
-print G.AdjacencyMatrix()
+#print G.AdjacencyMatrix()
 
 J = Graph(["a","b","c","d"],[("a","b"),("b","c"),("c","d")])
 print J.AdjacencyMatrix()
 
 K =Graph(["e","f","g","h"],[("e","g"),("g","f"),("f","h")])
 print K.AdjacencyMatrix()
-
+print J.IsIsomorphTo(G)
 
 #Grafo que  genera error.
 #H = Graph(["d","e","f"], [("a","b"),("e","f")])
 #print H.AdjacencyMatrix()
+
